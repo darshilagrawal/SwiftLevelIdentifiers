6d016ebc2390c565d9a805f0ab7ea7d00408c2c3
darshilagrawal <darshilagrawal2000@gmail.com>
2021-02-13 14:26:11 +0530

    Swift Learning Code
    A Program to check Access Levels of different swift variables and class objects

---
+++ SwiftAccessLevels/AnotherFile.swift
@@ -0,0 +1,14 @@
+
+import Foundation
+
+class AnotherClassInAnotherFile {
+    
+    init() {
+        
+        //Step 8. Try to print aFilePrivateProperty Here
+        
+        //Step 10. Try to print anInternalProperty Here
+        
+    }
+    
+}
---
+++ SwiftAccessLevels/main.swift
@@ -0,0 +1,9 @@
+
+
+import Foundation
+
+let aClass = AClass()
+
+aClass.methodA()
+
+
